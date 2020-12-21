a33867dd621883f14c857a098d16c1747b2c450e
abc <abc@example.com>
2020-12-21 15:18:01 +0530

    The starting Files

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+    return (
+        <div className="header"> 
+            <img className="header__logo" src="" />
+        </div>
+    )
+}
+
+ export default Header
